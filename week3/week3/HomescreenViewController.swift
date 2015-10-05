6328b3e90f920d7b7213b370902d048cd7264cc6
Angel Steger <angel.steger@gmail.com>
2015-10-05 00:41:30 -0700

    message slider working
    -sliding message view in each direction changes color and icon

--- week3/week3/HomescreenViewController.swift
+++ week3/week3/HomescreenViewController.swift
@@ -37,9 +37,11 @@ class HomescreenViewController: UIViewController {
         scrollView.contentSize = CGSize(width: 320, height: 1367)
         
         //Set initial values
+        laterIcon.alpha = 0
         listIcon.alpha = 0
+        archiveIcon.alpha = 0
         deleteIcon.alpha = 0
-        messageContainerView.backgroundColor = UIColor.grayColor()
+        //messageContainerView.backgroundColor = UIColor.grayColor()
         
         originalMessageCenter = messageView.center
         originalLaterIconCenter = laterIcon.center
@@ -63,16 +65,63 @@ class HomescreenViewController: UIViewController {
         let velocity = sender.velocityInView(view)
         
         
+        //Begin panning, set the original center
         if sender.state == UIGestureRecognizerState.Began {
             originalMessageCenter.x = messageView.center.x
             print("Gesture began at: \(point)")
             
-            
+        //As the message is dragged, update the center to include the new translated distance
         } else if sender.state == UIGestureRecognizerState.Changed {
             messageView.center.x = CGFloat(originalMessageCenter.x + translation.x)
-            print("Gesture changed at: \(point)")
-            
-        } else if sender.state == UIGestureRecognizerState.Ended {
+          
+
+            //print("Gesture changed at: \(point)")
+            //print("message view center is: \(messageView.center)")
+
+            //left short swipe of 60pts or fewer, later icon doesn't move
+            if (messageView.center.x < 160 && messageView.center.x > 100) {
+                laterIcon.alpha = 0.5
+                listIcon.alpha = 0
+                messageContainerView.backgroundColor = UIColor(red: 226/255, green: 226/255, blue: 226/255, alpha: 1)
+            }
+            //left swipe between 60 and 260pts, later icon moves with message view
+            else if (messageView.center.x < 100 && messageView.center.x > -100) {
+                laterIcon.alpha = 1
+                laterIcon.center.x = messageView.center.x + 190
+                listIcon.alpha = 0
+                messageContainerView.backgroundColor = UIColor(red: 250/255, green: 211/255, blue: 51/255, alpha: 1)
+            }
+            //left swipe between 260 and 320pts, change to list icon
+            else if (messageView.center.x < -100 && messageView.center.x > -160) {
+                laterIcon.alpha = 0
+                listIcon.alpha = 1
+                listIcon.center.x = messageView.center.x + 190
+                messageContainerView.backgroundColor = UIColor(red: 216/255, green: 166/255, blue: 117/255, alpha: 1)
+            }
+            //right swipe of 60pts or fewer, archive icon doesn't move
+            else if (messageView.center.x > 160 && messageView.center.x < 220) {
+                archiveIcon.alpha = 0.5
+                deleteIcon.alpha = 0
+                messageContainerView.backgroundColor = UIColor(red: 226/255, green: 226/255, blue: 226/255, alpha: 1)
+            }
+            //right swipe between 60pts and 260pts, archive icon moves with message view
+            else if (messageView.center.x > 220 && messageView.center.x < 420) {
+                archiveIcon.alpha = 1
+                archiveIcon.center.x = messageView.center.x - 190
+                deleteIcon.alpha = 0
+                messageContainerView.backgroundColor = UIColor(red: 112/255, green: 217/255, blue: 98/255, alpha: 1)
+            }
+            //right swipe between 260pts and 320pts, change to delete icon
+            else if (messageView.center.x > 420 && messageView.center.x < 480) {
+                archiveIcon.alpha = 0
+                deleteIcon.alpha = 1
+                deleteIcon.center.x = messageView.center.x - 190
+                messageContainerView.backgroundColor = UIColor(red: 235/255, green: 84/255, blue: 51/255, alpha: 1)
+            }
+        }
+        
+        
+        else if sender.state == UIGestureRecognizerState.Ended {
             print("Gesture ended at: \(point)")
         }
     }
